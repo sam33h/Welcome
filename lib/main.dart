6daf7e17f115ceaf22df67020fa5478dbfc76b50
sam33h <sameehv23@gmail.com>
2025-07-04 17:09:37 +0530

    Firebase installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:signup/login.dart';
 import 'package:signup/signup.dart';
 import 'package:signup/wlcm.dart';
 
+import 'firebase_options.dart';
 
-void main(){
+
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(home: wlcm(), debugShowCheckedModeBanner: false,));
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:signup/login.dart';
 
-class signup extends StatelessWidget {
+class signup extends StatefulWidget {
   const signup({super.key});
+
+  @override
+  State<signup> createState() => _signupState();
+}
+
+class _signupState extends State<signup> {
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  TextEditingController confirmcontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +28,8 @@ class signup extends StatelessWidget {
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
               ),
               SizedBox(height: 25),
-              Text("Create an account, its's free",
+              Text(
+                "Create an account, its's free",
                 style: TextStyle(color: Colors.grey),
               ),
               SizedBox(height: 25),
@@ -28,73 +39,90 @@ class signup extends StatelessWidget {
                   fillColor: Colors.white,
                   labelText: "Username",
                   border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: BorderSide(color: Colors.black)),
-
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
                 ),
               ),
 
-
               SizedBox(height: 25),
               TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    labelText: "Email",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: "Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide(color: Colors.black),
                   ),
                 ),
-              SizedBox(height: 25),
-               TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    labelText: "Password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                  ),
-                ),
+              ),
               SizedBox(height: 25),
               TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    labelText: "Confirm Password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
+                obscureText: true,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide(color: Colors.black),
                   ),
                 ),
+              ),
+              SizedBox(height: 25),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelText: "Confirm Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
               SizedBox(height: 25),
 
-              SizedBox(height: 40, width: double.infinity,
+              SizedBox(
+                height: 40,
+                width: double.infinity,
                 child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Signup"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      foregroundColor: Colors.white,
-                      side: BorderSide(color: Colors.black),
+                  onPressed: () {},
+                  child: Text("Signup"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    foregroundColor: Colors.white,
+                    side: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(fontSize: 21, color: Colors.grey),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => login()),
+                      );
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
+                ],
               ),
-              SizedBox(height: 50,),
-              Row(mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Already have an account?",
-                    style: TextStyle(fontSize: 21, color: Colors.grey),),
-                  TextButton(onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => login(),));},
-                      child: Text("Login", style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),))
-                ],)
             ],
           ),
         ),
