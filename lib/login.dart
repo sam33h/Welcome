388df3e9d939997fdd16cbd16f0cdea9fbc38f03
sam33h <sameehv23@gmail.com>
2025-07-07 16:43:41 +0530

    validation implemented

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:signup/forgotpassword.dart';
 import 'package:signup/services.dart';
@@ -11,110 +13,147 @@ class login extends StatefulWidget {
 }
 
 class _loginState extends State<login> {
+  bool visible = false;
   TextEditingController emailcontroller = TextEditingController();
   TextEditingController passwordcontroller = TextEditingController();
+  final formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 600, right: 600),
-          child: Column(
-            children: [
-              SizedBox(height: 50),
-              Text(
-                "Login",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-              ),
-              SizedBox(height: 25),
-              TextFormField(
-                controller: emailcontroller,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  labelText: "Email",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Form(
+        key: formkey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 600, right: 600),
+            child: Column(
+              children: [
+                SizedBox(height: 50),
+                Text(
+                  "Login",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
                 ),
-              ),
-
-              SizedBox(height: 25),
-              TextFormField(
-                controller: passwordcontroller,
-                obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  labelText: "Password",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                ),
-              ),
-              GestureDetector(
-                child: Align(
-                  child: Text("Forgot password?"),
-                  alignment: Alignment.bottomRight,
-                ), onTap: () {
-                Navigator.push(context,MaterialPageRoute(builder:(context)=>Forpassword()) );
-                },),
-
-              SizedBox(height: 20),
-              SizedBox(
-                height: 40,
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    loginn(
-                      email: emailcontroller.text,
-                      password: passwordcontroller.text,
-                      context: context,
-                    );
+                SizedBox(height: 25),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Email required";
+                    }
+                    if (!value.contains("@")) {
+                      return "Email invalid";
+                    }
                   },
-                  child: Text("Login"),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.white,
-                  ),
-                ),
-              ),
-              SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account?",
-                    style: TextStyle(fontSize: 21, color: Colors.grey),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => signup()),
-                      );
-                    },
-                    child: Text(
-                      "Sign up",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
+                  controller: emailcontroller,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: Colors.black),
                     ),
                   ),
-                ],
-              ),
-
-              SizedBox(height: 50),
-              Image(
-                image: NetworkImage(
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ50yhgzSsaGyvGGysr7uHIn_j5Pwh5OgClQg&s",
                 ),
-              ),
-            ],
+
+                SizedBox(height: 25),
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Enter password";
+                    }
+                    return null;
+                  },
+                  controller: passwordcontroller,
+                  obscureText: !visible,
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          visible=!visible;
+                        });
+                      },
+                      icon: visible
+                          ? Icon(Icons.visibility)
+                          : Icon(Icons.visibility_off),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Align(
+                    child: Text("Forgot password?",style: TextStyle(color: Colors.lightBlue),),
+                    alignment: Alignment.bottomRight,
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Forpassword()),
+                    );
+                  },
+                ),
+
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (formkey.currentState!.validate()) {
+                        loginn(
+                          email: emailcontroller.text,
+                          password: passwordcontroller.text,
+                          context: context,
+                        );
+                      }
+                    },
+                    child: Text("Login"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(fontSize: 21, color: Colors.grey),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => signup()),
+                        );
+                      },
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 50),
+                Image(
+                  image: NetworkImage(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ50yhgzSsaGyvGGysr7uHIn_j5Pwh5OgClQg&s",
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/signup.dart
+++ lib/signup.dart
@@ -14,117 +14,181 @@ class _signupState extends State<signup> {
   TextEditingController emailcontroller = TextEditingController();
   TextEditingController passwordcontroller = TextEditingController();
   TextEditingController confirmcontroller = TextEditingController();
+  final formkey = GlobalKey<FormState>();
+  bool visible=false;
+  bool ivisible=false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 600, right: 600),
-          child: Column(
-            children: [
-              SizedBox(height: 50),
-              Text(
-                "Sign up",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-              ),
-              SizedBox(height: 25),
-              Text(
-                "Create an account, its's free",
-                style: TextStyle(color: Colors.grey),
-              ),
-              SizedBox(height: 25),
-              TextFormField( controller: namecontroller,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  labelText: "Username",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Form(key: formkey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 600, right: 600),
+            child: Column(
+              children: [
+                SizedBox(height: 50),
+                Text(
+                  "Sign up",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
                 ),
-              ),
-
-              SizedBox(height: 25),
-              TextFormField( controller: emailcontroller,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  labelText: "Email",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
+                SizedBox(height: 25),
+                Text(
+                  "Create an account, its's free",
+                  style: TextStyle(color: Colors.grey),
                 ),
-              ),
-              SizedBox(height: 25),
-              TextFormField(controller: passwordcontroller,
-                obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  labelText: "Password",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                ),
-              ),
-              SizedBox(height: 25),
-              TextFormField(controller: confirmcontroller,
-                obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  labelText: "Confirm Password",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                ),
-              ),
-              SizedBox(height: 25),
-
-              SizedBox(
-                height: 40,
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {signupp(username: namecontroller.text, email: emailcontroller.text, password: passwordcontroller.text, confirm: confirmcontroller.text, context: context);},
-                  child: Text("Signup"),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,
-                    foregroundColor: Colors.white,
-                    side: BorderSide(color: Colors.black),
-                  ),
-                ),
-              ),
-              SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Already have an account?",
-                    style: TextStyle(fontSize: 15, color: Colors.grey),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => login()),
-                      );
-                    },
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
+                SizedBox(height: 25),
+                TextFormField(
+                  controller: namecontroller,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Enter Username";
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: "Username",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: Colors.black),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+
+                SizedBox(height: 25),
+                TextFormField(
+                  controller: emailcontroller,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return " Please Enter an Email";
+                    }
+                    if (!value.contains("@")) {
+                      return "Please enter valid email";
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25),
+                TextFormField(
+                  controller: passwordcontroller,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Password required";
+                    }
+                    return null;
+                  },
+                  obscureText: !ivisible,
+                  decoration: InputDecoration(suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        ivisible=!ivisible;
+                      });
+                    },
+                    icon: ivisible
+                        ? Icon(Icons.visibility)
+                        : Icon(Icons.visibility_off),
+                  ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25),
+                TextFormField(
+                  controller: confirmcontroller,
+                  validator: (value) {
+                    if (value != passwordcontroller.text) {
+                      return "Enter the same password";
+                    }
+                    return null;
+                  },
+                  obscureText: !visible,
+                  decoration: InputDecoration(
+                      suffixIcon: IconButton( onPressed: () {
+                    setState(() {
+                      visible=!visible;
+                    });
+                  },
+                    icon: visible
+                        ? Icon(Icons.visibility)
+                        : Icon(Icons.visibility_off),
+                  ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: "Confirm Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25),
+
+                SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if(formkey.currentState!.validate()){
+                      signupp(
+                        username: namecontroller.text,
+                        email: emailcontroller.text,
+                        password: passwordcontroller.text,
+                        confirm: confirmcontroller.text,
+                        context: context,
+                      );
+                    }},
+                    child: Text("Signup"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                      foregroundColor: Colors.white,
+                      side: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account?",
+                      style: TextStyle(fontSize: 15, color: Colors.grey),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => login()),
+                        );
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
